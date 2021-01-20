25a2305d008c89b0bb4de08bb4ba268ea233e541
Gabester0 <gceipper@gmail.com>
2021-01-20 03:08:29 -0500

    implementing and testing linkedlist sort

--- build/index.js
+++ build/index.js
@@ -1,12 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
-var CharactersCollections_1 = require("./CharactersCollections");
+var LinkedLists_1 = require("./LinkedLists");
 // const numbersCollection = new NumbersCollection([10, 3, -35, 110])
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(sorter.collection)
-var charactersCollection = new CharactersCollections_1.CharactersCollection("AxggEFSfse");
-var sorter = new Sorter_1.Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection(`AxggEFSfse`)
+// const sorter = new Sorter(charactersCollection)
+// sorter.sort()
+// console.log(charactersCollection.data)
+var linkedList = new LinkedLists_1.LinkedList();
+linkedList.add(4502);
+linkedList.add(-345);
+linkedList.add(25);
+linkedList.add(-34);
+linkedList.add(4503);
+var sorter = new Sorter_1.Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollections'
+import { LinkedList } from './LinkedLists';
 
 // const numbersCollection = new NumbersCollection([10, 3, -35, 110])
 // const sorter = new Sorter(numbersCollection);
 // sorter.sort();
 // console.log(sorter.collection)
 
-const charactersCollection = new CharactersCollection(`AxggEFSfse`)
-const sorter = new Sorter(charactersCollection)
-sorter.sort()
-console.log(charactersCollection.data)
+// const charactersCollection = new CharactersCollection(`AxggEFSfse`)
+// const sorter = new Sorter(charactersCollection)
+// sorter.sort()
+// console.log(charactersCollection.data)
+
+const linkedList = new LinkedList();
+linkedList.add(4502);
+linkedList.add(-345);
+linkedList.add(25);
+linkedList.add(-34);
+linkedList.add(4503);
+
+const sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
